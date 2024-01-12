ad1187c6bd8167e6e659bd60dbf6edffdeb5d8a1
gamza2695 <127659111+gamza2695@users.noreply.github.com>
2024-01-12 10:08:37 +0900

    [level Lv.0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+		for(int i = 0; i<my_string.length();i++) {
+			if(my_string.charAt(i)<65) answer+= my_string.charAt(i) - '0';
+		}        
+        return answer;
+    }
+}
