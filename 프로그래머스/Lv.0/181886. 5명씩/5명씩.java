fee3bf0cd2fae8af47af4c2df646f233f1ab9385
gamza2695 <127659111+gamza2695@users.noreply.github.com>
2024-01-16 09:24:35 +0900

    [level Lv.0] Title: 5명씩, Time: 0.27 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181886. 5명씩/5명씩.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(int)Math.ceil(names.length/5.0)];
+
+        for(int i=0; i<answer.length; i++){
+            answer[i] = names[5*i];
+        }
+        
+        return answer;
+    }
+}
