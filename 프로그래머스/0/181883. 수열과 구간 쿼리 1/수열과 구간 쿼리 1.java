5d41bf09da71122d48d722e20cedf7e0b8bbc499
Yoojin <127659111+gamza2695@users.noreply.github.com>
2024-01-23 11:27:10 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int[] query : queries){
+            
+            int s = query[0];
+            int e = query[1];
+            
+            for(int i=s; i<=e; i++){
+                arr[i]++;
+            }
+        }
+        
+        return arr;
+    }
+}
