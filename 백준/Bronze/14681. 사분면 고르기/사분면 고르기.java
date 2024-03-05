4fd81681b9f1fa5ea53651a0e0afb0c5fa48a173
Yoojin <127659111+gamza2695@users.noreply.github.com>
2024-03-05 13:52:01 +0900

    [Bronze V] Title: 사분면 고르기, Time: 200 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        if(x>0) System.out.println(y>0? 1 : 4);
+        else System.out.println(y>0? 2 : 3);
+	}
+}
