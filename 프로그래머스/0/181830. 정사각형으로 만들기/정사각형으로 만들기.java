015ba0975e792719be8d0614c2f2d2a8d3040d59
Yoojin <127659111+yj2695@users.noreply.github.com>
2024-05-03 10:48:36 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.28 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+		int[][] answer = arr.length>arr[0].length? new int[arr.length][arr.length] : new int[arr[0].length][arr[0].length];
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[0].length; j++) {
+				answer[i][j] = arr[i][j];
+			}
+		}
+        
+        return answer;
+    }
+}
